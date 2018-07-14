cc16e23c8d923347426e8f9aef04f989ffde0bb3
Kenny Chung <kcmac@Kennys-MacBook-Pro.local>
2018-07-13 17:13:08 -1000

    planet and person fully functional

---
+++ js/app.js
@@ -0,0 +1,97 @@
+'use strict'
+const requestButton = document.getElementById("requestResourceButton");
+const input = document.getElementById('resourceId');
+const selector = document.getElementById('resourceType');
+const contentContainer = document.getElementById('contentContainer');
+
+const contentDiv = document.createElement('div');
+contentDiv.id = 'bio';
+contentContainer.appendChild(contentDiv);
+
+const requestedName = document.createElement('h2');
+requestedName.id = 'name';
+contentDiv.appendChild(requestedName);
+
+const requestedAttribute = document.createElement('p');
+requestedAttribute.id = 'gender-terrain-manufactuerer';
+contentDiv.appendChild(requestedAttribute);
+
+const requestedMisc = document.createElement('p');
+requestedMisc.id = 'species-population-starshipclass';
+contentDiv.appendChild(requestedMisc);
+
+const requestedFilmList = document.createElement('ul');
+requestedFilmList.id = 'appearances';
+contentDiv.appendChild(requestedFilmList);
+
+const filmHeader = document.createElement('h3');
+filmHeader.id = 'appears-in';
+requestedFilmList.appendChild(filmHeader);
+
+requestButton.addEventListener('click', function(){
+  
+  if (selector.value === 'people'){
+    const getPersonInfo = new XMLHttpRequest();
+    getPersonInfo.open('GET', "https://swapi.co/api/people/" + input.value +'/');
+    getPersonInfo.send();
+    
+    getPersonInfo.addEventListener('load', function(){
+      const parsedPerson = JSON.parse(this.responseText);
+      
+      const getSpecies = new XMLHttpRequest();
+      getSpecies.open('GET', parsedPerson.species[0]);
+      getSpecies.send();
+      getSpecies.addEventListener('load', function(){
+        const parsedSpecies = JSON.parse(this.responseText);
+        
+        requestedName.innerHTML = 'Name: ' + parsedPerson.name;
+        requestedAttribute.innerHTML = 'Gender: ' + parsedPerson.gender;
+        requestedMisc.innerHTML = 'Species: ' + parsedSpecies.name;
+        requestedFilmList.innerHTML = '';
+      })
+    });
+
+  } else if (selector.value === 'planets'){
+    const getPlanetInfo = new XMLHttpRequest();
+    getPlanetInfo.open('GET', "https://swapi.co/api/planets/" + input.value + '/');
+    getPlanetInfo.send();
+
+    getPlanetInfo.addEventListener('load', function() {
+      const parsedPlanet = JSON.parse(this.responseText);
+
+      
+      for (let i = 0; i < parsedPlanet.films.length; i++){
+        const getFilmAppearances = new XMLHttpRequest();
+        getFilmAppearances.open('GET', parsedPlanet.films[i]);
+        getFilmAppearances.send();
+        
+        getFilmAppearances.addEventListener('load', function(){
+          const parsedFilms = JSON.parse(this.responseText);
+          
+          filmHeader.innerHTML = 'Appears in: ';
+          const createList = document.createElement('li');
+          requestedFilmList.appendChild(createList);
+          createList.innerHTML = parsedFilms.title;
+          
+          requestedName.innerHTML = 'Name: ' + parsedPlanet.name;
+          requestedAttribute.innerHTML = 'Terrain: ' + parsedPlanet.terrain;
+          requestedMisc.innerHTML = 'Population: ' + parsedPlanet.population;
+        })
+      }
+      requestedFilmList.innerHTML = '';
+    })
+
+  } else if (selector.value === 'starships'){
+    const getStarshipInfo = new XMLHttpRequest();
+    getStarshipInfo.open('GET', 'https://swapi.co/api/starships/' + input.value + '/');
+    getStarshipInfo.send();
+
+    getStarshipInfo.addEventListener('load', function(){
+      const parsedStarship = JSON.parse(this.responseText);
+
+      requestedName.innerHTML = 'Name: ' + parsedStarship.name;
+      requestedAttribute.innerHTML = 'Manufacturer: ' + parsedStarship.manufacturer;
+      requestedMisc.innerHTML = 'Starship Class: ' + parsedStarship.starship_class;
+    })
+  }
+});
